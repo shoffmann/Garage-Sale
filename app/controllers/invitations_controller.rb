870ecd82e5cf3c451de81dae270e0df8019eef94
Steve Hoffmann <shoffmann@bendbulletin.com>
2009-11-04 11:16:18 -0800

    Fixed invitation show when only email address invited

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -5,21 +5,8 @@ class InvitationsController < ApplicationController
   end
   
   def new
-  	if should_update_profile?
-  	  update_profile
-  	end
-
     list_of_friends = (params[:fb_sig_friends]||"").split(/,/)
     @not_potential_disciples = current_user.friends_with_items(list_of_friends).map(&:facebook_id)
-  end
-  
-  def should_update_profile?
-    params[:from]
-  end
-  
-  def update_profile
-  	@user = facebook_session.user
-  	@user.profile_fbml = render_to_string(:partial=>"profile", :locals=>{:from=>params[:from]})	  
   end
   
   def create
