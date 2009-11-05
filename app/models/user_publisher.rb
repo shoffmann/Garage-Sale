2b4340d0fe93c95761428e66632e5449ed421a6d
Steve Hoffmann <shoffmann@bendbulletin.com>
2009-11-05 09:58:08 -0800

    Made short story wall post better

--- app/models/user_publisher.rb
+++ app/models/user_publisher.rb
@@ -1,55 +1,30 @@
 class UserPublisher < Facebooker::Rails::Publisher
 
-# Item notifications #
-  def item_notification(item)
-    send_as :notification
-    recipients  item.user_id
-    from current_user
-    fbml  <<-MESSAGE
-  	  <fb:fbml> 
-  	  #{name item.user_id} commented on #{item}.
-  	  #{link_to "Message them back", item_url}
-  	  </fb:fbml>
-  	MESSAGE
-  end
-  
-  def item_notification_email(item)
-    send_as :email
-    recipients  item.user_id
-    from current_user
-    title "Someone is interested in your item"
-    fbml  <<-MESSAGE
-  	  <fb:fbml> 
-  	  #{name item.user_id} commented on #{item}.
-  	  #{link_to "Message them back", item_url}
-  	  </fb:fbml>
-  	MESSAGE
-  end
+  # User Profile Updates
+    def profile_update(user)
+      send_as :profile
+      recipients user
+      profile render(:partial=>"users/profile_boxes", 
+                     :assigns=>{:user=>user})
+      profile_main render(:partial=>"users/profile", 
+                          :assigns=>{:user=>user})
+    end
 
 # Item Wall Feeds
   def item_template
     one_line_story_template "{*actor*} added {*new_item*} to {*userlink*}."
     one_line_story_template "{*actor*} added items to Garage Sale"
-    short_story_template "{*actor*} added {*new_item*} to {*userlink*}.", render(:partial=>"short_body")
+    short_story_template "added {*new_item*} to {*userlink*}.", render(:partial=>"short_body")
   end
   
   def item(item)
     send_as :user_action
     from item.user.facebook_session.user
+    story_size SHORT or ONE_LINE
     data  :new_item => link_to(item.title, item_url(item)),
           :item_desc => item.description,
           :userlink => link_to("Garage Sale", items_url(item.user)),
-          :applink => link_to("Garage Sale", items_path)
+          :applink => link_to("Garage Sale", items_url)
   end
 
-# User Profile Updates
-  def profile_update(user)
-    send_as :profile
-    recipients user
-    profile render(:partial=>"users/profile_boxes", 
-                   :assigns=>{:user=>user})
-    profile_main render(:partial=>"users/profile", 
-                        :assigns=>{:user=>user})
-  end
-    
 end
